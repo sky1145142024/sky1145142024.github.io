d7befe4beca9c8290233456ad7fceb129ddad57f
sky1145142024 <2076780485@qq.com>
2025-10-19 16:08:29 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,22 +1,15 @@
 const config = {
   firebase: {
- apiKey: "AIzaSyA7hNGBhJAe34tybE0iCphlps_4AP78Gpg",
-  authDomain: "pideweb-50b61.firebaseapp.com",
-  projectId: "pideweb-50b61",
-  storageBucket: "pideweb-50b61.firebasestorage.app",
-  messagingSenderId: "600392139713",
-  appId: "1:600392139713:web:2627688a5fbba20fc5a540",
-  measurementId: "G-ZS7HXH1F72"
-  },
-  github: {
-    clientId: "Ov23liUHw8DobU7utxCH", // 从 GitHub OAuth 应用获取
-    redirectUri: "https://sky1145142024.github.io/login.html" // 回调URL
-  }
+  apiKey: "AIzaSyAiLmlpBzXmjADzOUEJicIJt6gTk1XLfrY",
+  authDomain: "task-platform-2024.firebaseapp.com",
+  projectId: "task-platform-2024",
+  storageBucket: "task-platform-2024.firebasestorage.app",
+  messagingSenderId: "90420240419",
+  appId: "1:90420240419:web:9bac41ecf8726304855196"
+  };
+const githubConfig = {
+  clientId: "Ov23liUHw8DobU7utxCH", // 你的GitHub Client ID
+  redirectUri: "https://sky1145142024.github.io/login.html" // 回调地址
 };
-
-// 初始化 Firebase
-firebase.initializeApp(config.firebase);
-const db = firebase.firestore();
-const auth = firebase.auth();
 
 
