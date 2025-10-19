6b69133fd61edc07a8d3bca078e23c8730b6e88b
sky1145142024 <2076780485@qq.com>
2025-10-19 16:12:25 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,15 +1,14 @@
-const config = {
-  firebase: {
+// js/config.js（直接定义 firebaseConfig 变量，去掉外层 config）
+const firebaseConfig = {
   apiKey: "AIzaSyAiLmlpBzXmjADzOUEJicIJt6gTk1XLfrY",
   authDomain: "task-platform-2024.firebaseapp.com",
   projectId: "task-platform-2024",
   storageBucket: "task-platform-2024.firebasestorage.app",
   messagingSenderId: "90420240419",
   appId: "1:90420240419:web:9bac41ecf8726304855196"
-  };
-const githubConfig = {
-  clientId: "Ov23liUHw8DobU7utxCH", // 你的GitHub Client ID
-  redirectUri: "https://sky1145142024.github.io/login.html" // 回调地址
 };
 
-
+const githubConfig = {
+  clientId: "Ov23liUHw8DobU7utxCH",
+  redirectUri: "https://sky1145142024.github.io/login.html"
+};
