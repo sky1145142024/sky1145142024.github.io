552be413cfc50b0445bd89d140fb39dd073f1207
sky1145142024 <2076780485@qq.com>
2025-10-19 13:32:52 +0800

    Update config.js

---
+++ js/config.js
@@ -0,0 +1,19 @@
+const config = {
+  firebase: {
+ apiKey: "AIzaSyA7hNGBhJAe34tybE0iCphlps_4AP78Gpg",
+  authDomain: "pideweb-50b61.firebaseapp.com",
+  projectId: "pideweb-50b61",
+  storageBucket: "pideweb-50b61.firebasestorage.app",
+  messagingSenderId: "600392139713",
+  appId: "1:600392139713:web:2627688a5fbba20fc5a540",
+  },
+  github: {
+    clientId: "Ov23liUHw8DobU7utxCH", // 从 GitHub OAuth 应用获取
+    redirectUri: "https://sky1145142024.github.io/-web/login.html" // 回调URL
+  }
+};
+
+// 初始化 Firebase
+firebase.initializeApp(config.firebase);
+const db = firebase.firestore();
+const auth = firebase.auth();
